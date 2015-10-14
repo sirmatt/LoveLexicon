63200d3d44db599db56d4c0ea552eb3b543c2615
Matt Batista <mattbgroovy@gmail.com>
2015-10-14 09:32:27 -0700

    Fix minor tweaks to user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: "sessions#new"
+  root to: "static_pages#root"
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
 end
