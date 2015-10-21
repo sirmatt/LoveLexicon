759aef1e835c9ffd67c10ced2e7774fa9f7de338
Matt Batista <mattbgroovy@gmail.com>
2015-10-21 10:43:07 -0700

    Fix stylesheets

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'react-rails', '~> 1.3.0'
+gem 'flux-rails-assets'
 # Use Unicorn as the app server
 # gem 'unicorn'
 
@@ -41,8 +43,6 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
-  gem 'react-rails', '~> 1.3.0'
-  gem 'flux-rails-assets'
   gem 'annotate'
   gem 'better_errors'
   gem 'binding_of_caller'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-matt = User.create!(username: "matt", password: "password")
+matt = User.create!(username: "Matt", password: "password")
+roxanne = User.create!(username: "Roxanne", password: "butterfly")
+tom = User.create!(username: "Tom", password: "shrimp")
+shelly = User.create!(username: "Shelly", password: "bagel1")
 
-word1 = Definition.create(author_id: 1, word: "apple-bottom", word_def: "Jeans, boots with the fur")
+word1 = Definition.create(author_id: 1, word: "I'll call you", word_def: "I’m not going to call you. Well, I might. Probably not, though.")
+word2 = Definition.create(author_id: 2, word: "apple-bottom", word_def: "Jeans, boots with the fur")
+word3 = Definition.create(author_id: 3, word: "apple-bottom", word_def: "Jeans, boots with the fur")
+word4 = Definition.create(author_id: 4, word: "apple-bottom", word_def: "Jeans, boots with the fur")
+word5 = Definition.create(author_id: 2, word: "apple-bottom", word_def: "Jeans, boots with the fur")
+word6 = Definition.create(author_id: 3, word: "apple-bottom", word_def: "Jeans, boots with the fur")
+word7 = Definition.create(author_id: 4, word: "apple-bottom", word_def: "Jeans, boots with the fur")
+word8 = Definition.create(author_id: 1, word: "apple-bottom", word_def: "Jeans, boots with the fur")
