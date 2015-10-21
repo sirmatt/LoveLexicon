fe1b02c2044aac94af29a10ac37d6d83459b50a4
Matt Batista <mattbgroovy@gmail.com>
2015-10-21 10:48:25 -0700

    Fix application.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,12 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require react
 //= require react_ujs
 //= require reactrouter.min.js
 //= require components
-//= require bootstrap-sprockets
 //= require flux
 //= require eventemitter
 //= require_tree .
